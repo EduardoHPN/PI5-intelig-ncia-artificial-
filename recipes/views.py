6683aa6f38327db87a4881c6f54df5fb8665b32d
Eduardo Henrique <du.nemesio@gmail.com>
2025-01-15 11:19:44 -0300

    html incial impression

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(resquest):
-    return render(resquest, 'recipes/home.html',{
+    return render(resquest, 'recipes/pages/home.html',{
         'name': "Eduardo Henrique Porto Nemesio"
     })
 
-def contato(request):
-    return HttpResponse("+55(19)99949-8012")
-
-def sobre(request):
-    return HttpResponse("Eduardo Henrique Porto Nemesio")
