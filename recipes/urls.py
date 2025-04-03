7628431ec6e66fe60cd2a2bcf6626590d867a3f3
Eduardo Henrique <du.nemesio@gmail.com>
2025-04-03 16:50:19 -0300

    new modifies

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from recipes.views import home, Penal
+from recipes.views import home, Penal, EmBreve
 
 
 urlpatterns = [
     path('', home),
-    path('recipes/3/', Penal)
+    path('recipes/3/', Penal),
+    path('recipes/2/', EmBreve),
+    path('recipes/1/', EmBreve)
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 
 def home(resquest):
-    return render(resquest, 'recipes/pages/home.html',{
-    })
+    return render(resquest, 'recipes/pages/home.html',{})
 
 def Penal(request):
-    return render(request, 'recipes/pages/penal_home.html',{}
-)
+    return render(request, 'recipes/pages/penal_home.html',{})
+
+def EmBreve(request):
+    return render(request, 'recipes/pages/emBreve.html', {})
