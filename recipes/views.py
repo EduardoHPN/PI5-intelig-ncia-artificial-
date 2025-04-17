c45fd3168ba711c1398e0476837ba99a2be4bf26
Eduardo Henrique <du.nemesio@gmail.com>
2025-04-17 08:21:42 -0300

    feature/blockHomeBeforeLogin

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
+import json
 
 
-def home(resquest):
-    return render(resquest, 'recipes/pages/home.html',{})
+@csrf_exempt
+def autenticar_usuario(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            uid = data.get('uid')
+
+            if uid:
+                print(f'UID recebido: {uid}')
+                request.session['autenticado'] = True
+                request.session['uid'] = uid 
+                return HttpResponseRedirect('/home/')
+            else:
+                return JsonResponse({'error': 'UID não fornecido'}, status=400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'JSON inválido'}, status=400)
+
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+
+
+def home(request):
+    uid = request.session.get('uid')
+    if not request.session.get('autenticado'):
+        return redirect('/')
+    print(f"UID do usuário autenticado: {uid}")
+    return render(request, 'recipes/pages/home.html', {})
 
 def Penal(request):
     return render(request, 'recipes/pages/penal_home.html',{})
