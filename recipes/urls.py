314d14e119298675cd97d325895cdf757875d50e
Lucas <lucas.clopes04@gmail.com>
2025-05-15 16:51:11 -0300

    estilizacao forms ---- antes de criar div nova(argumentacao juridica)

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path('', Login),
     path('logout/', logout),
     path('cadastro/', Cadastro),
-    path('recipes/3/', Penal),
+    path('recipes/3/', Penal), #form 1
     path('recipes/2/', EmBreve),
     path('recipes/1/', EmBreve),
     path('autenticar/', autenticar_usuario),
-    path('recipes/arg-juridica/', ArgJuridica, name='argju'),
-    path('recipes/pedido/', pedido, name='pedido'),
-    path('recipes/documentacao/', documentos, name='documentos'),
-    path('recipes/defesa-preliminar/', defesa_preliminar ,name='preliminar') # nova rota
+    path('recipes/arg-juridica/', ArgJuridica, name='argju'),#form 3
+    path('recipes/pedido/', pedido, name='pedido'),#form 4
+    path('recipes/documentacao/', documentos, name='documentos'),#form 5
+    path('recipes/defesa-preliminar/', defesa_preliminar ,name='preliminar') # nova rota #form 2
 ]
